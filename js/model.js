34a7c8108d00589700c86365ea33123b68ab454c
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-03-16 14:26:24 +0100

    days,weeks,months,years

--- js/model.js
+++ js/model.js
@@ -1,4 +1,21 @@
 // Models
 const model = {
+    months: [
+        'Januar',
+        'Februar',
+        'Mars',
+        'April',
+        'Mai',
+        'Juni',
+        'Juli',
+        'August',
+        'September',
+        'Oktober',
+        'November',
+        'Desember',
+    ],
+    days: [],
+    years: [], 
+    weeks: [],
 
 }
