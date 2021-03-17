8a961e41136ada38c684acf7412dda8297abbb22
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-03-17 14:05:01 +0100

    calender test

--- js/components/calender.js
+++ js/components/calender.js
@@ -1,8 +1,30 @@
-// for(let i = 0; i < 6; i++) {
-//     x = 30;
-//     for(let j = 0; j < 7; j++) {
-//         days(x, y, 20);
-//         x += 30;
-//     }
-//     y += 30;
-// }
+var daysOfWeek = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag"];
+
+daysOfWeek.push("Lørdag")
+daysOfWeek.unshift("Søndag")
+
+
+var table = "<table border=1><tr>";
+for(var i = 0; i < daysOfWeek.length; i++){
+    table += "<th>"+daysOfWeek[i]+"</th>";
+}
+table += "</tr>";
+
+var days = 30;
+var appendEmptyDays = 7 - (days+7) % 7;
+
+table += "<tr>";
+
+//Days and Weeks
+for(var i=1; i<=days; i++){
+    table += "<td>"+ i +"</td>";
+    if(i%7===0) table += "</tr><tr>";
+}
+
+for(var i=0; i<appendEmptyDays; i++){
+    table += "<td></td>";
+}
+
+table += "</tr></table>"
+
+document.body.innerHTML = table;
